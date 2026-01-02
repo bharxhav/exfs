a5fc48ac4945a2ad42bb2093710fd8bf53467432
Bhargav Kantheti <k@bk.bio>
2026-01-02 07:27:39 -0600

    RM all

--- exfs_views/xfs/cmd/root/flags.go
+++
@@ -1,8 +0,0 @@
-package root
-
-// Verbose enables verbose output across all commands.
-var Verbose bool
-
-func registerGlobalFlags() {
-	Cmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "V", false, "enable verbose output")
-}
--- exfs_views/xfs/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/bharxhav/exfs/cmd/root"
-)
-
-func main() {
-	root.Execute()
-}
